2d0351e57f4d3ab1cda808dd6efb1be0bfe9f9d9
chensley0216 <100445222+chensley0216@users.noreply.github.com>
2022-03-18 16:10:17 -0400

    Adding files to election_analysis

--- PyPoll.py
+++ PyPoll.py
@@ -15,7 +15,7 @@ file_to_save = os.path.join("analysis", "election_analysis,txt")
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
     # To do: read and analyze the data here.
-    # Read the file objext with the reader function.
+    # Read the file object with the reader function.
     file_reader = csv.reader(election_data)
     # Print the header row
     headers = next(file_reader)
